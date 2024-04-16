5dbf57489cc6cf1b8a7d35bc12fbfc83c5fc5a8c
Paulo <pauloramos.04@hotmail.com>
2024-04-16 17:52:13 -0300

    feat: Initializing project

---
+++ src/main/java/com/example/GiftHub/GiftHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.GiftHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftHubApplication.class, args);
+	}
+
+}
