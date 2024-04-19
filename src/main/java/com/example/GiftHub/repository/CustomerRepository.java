4a21f1ceb3c8c5a9d703710f6fe977628f675484
Paulo <pauloramos.04@hotmail.com>
2024-04-19 16:42:25 -0300

    feat: creating repository

---
+++ src/main/java/com/example/GiftHub/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.example.GiftHub.repository;
+
+
+import com.example.GiftHub.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
