0d123c8d42e16d3c98debd9e25bd6a412ef9a75c
Paulo <pauloramos.04@hotmail.com>
2024-04-19 16:43:03 -0300

    feat: creating product and custumer services

---
+++ src/main/java/com/example/GiftHub/service/CustomerService.java
@@ -0,0 +1,32 @@
+package com.example.GiftHub.service;
+
+import com.example.GiftHub.model.Customer;
+import com.example.GiftHub.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public Optional<Customer> getCustomerById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    public Customer saveCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public void deleteCustomer(Long id) {
+        customerRepository.deleteById(id);
+    }
+}
