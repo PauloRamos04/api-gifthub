ce8458c6e6a1ab11039c0b61ca5e5c4b81c17d1d
Paulo <pauloramos.04@hotmail.com>
2024-05-10 19:31:13 -0300

    chore: adding dependencies, database structure and docker

--- src/main/resources/db/migration/V1__create-database.sql
+++ src/main/resources/db/migration/V1__create-database.sql
@@ -1,17 +1,12 @@
-CREATE TABLE IF NOT EXISTS carrinho (
-  id_carrinho SERIAL PRIMARY KEY,
-  id_cliente INT NOT NULL,
-  id_produto INT NOT NULL,
-  quantidade INT NOT NULL,
-  CONSTRAINT fk_carrinho_cliente FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente),
-  CONSTRAINT fk_carrinho_produto FOREIGN KEY (id_produto) REFERENCES produto (id_produto)
+CREATE TABLE IF NOT EXISTS produto (
+    id_produto SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    preco NUMERIC(10, 2) NOT NULL,
+    estoque INT NOT NULL,
+    categoria VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS categoria (
-  id_categoria SERIAL PRIMARY KEY,
-  nome VARCHAR(50) NOT NULL,
-  descricao TEXT NOT NULL
-);
 
 CREATE TABLE IF NOT EXISTS cliente (
   id_cliente SERIAL PRIMARY KEY,
@@ -33,6 +28,17 @@ CREATE TABLE IF NOT EXISTS endereco (
   CONSTRAINT fk_endereco_cliente FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente)
 );
 
+
+CREATE TABLE IF NOT EXISTS produto (
+    id_produto SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    preco NUMERIC(10, 2) NOT NULL,
+    estoque INT NOT NULL,
+    categoria VARCHAR(50) NOT NULL
+);
+
+
 CREATE TABLE IF NOT EXISTS entrega (
   id_entrega SERIAL PRIMARY KEY,
   id_cliente INT NOT NULL,
@@ -44,3 +50,19 @@ CREATE TABLE IF NOT EXISTS entrega (
   CONSTRAINT fk_entrega_produto FOREIGN KEY (id_produto) REFERENCES produto (id_produto),
   CONSTRAINT fk_entrega_endereco FOREIGN KEY (id_endereco) REFERENCES endereco (id_endereco)
 );
+
+CREATE TABLE IF NOT EXISTS carrinho (
+  id_carrinho SERIAL PRIMARY KEY,
+  id_cliente INT NOT NULL,
+  id_produto INT NOT NULL,
+  quantidade INT NOT NULL,
+  CONSTRAINT fk_carrinho_cliente FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente),
+  CONSTRAINT fk_carrinho_produto FOREIGN KEY (id_produto) REFERENCES produto (id_produto)
+);
+
+CREATE TABLE IF NOT EXISTS categoria (
+  id_categoria SERIAL PRIMARY KEY,
+  nome VARCHAR(50) NOT NULL,
+  descricao TEXT NOT NULL
+);
+
