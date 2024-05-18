1fbd97fbf139df7c902d6e04204e98c4ce4f05dc
Paulo <pauloramos.04@hotmail.com>
2024-05-18 18:57:41 -0300

    refactor: alter database

--- src/main/resources/db/migration/V1__create-database.sql
+++ src/main/resources/db/migration/V1__create-database.sql
@@ -7,62 +7,41 @@ CREATE TABLE IF NOT EXISTS produto (
     categoria VARCHAR(50) NOT NULL
 );
 
-
-CREATE TABLE IF NOT EXISTS cliente (
-  id_cliente SERIAL PRIMARY KEY,
-  nome_completo VARCHAR(100) NOT NULL,
-  email VARCHAR(30) NOT NULL,
-  cpf VARCHAR(14) NOT NULL,
-  dt_nasc DATE,
-  senha VARCHAR(100)
-);
-
 CREATE TABLE IF NOT EXISTS endereco (
   id_endereco SERIAL PRIMARY KEY,
-  id_cliente INT NOT NULL,
   rua VARCHAR(50) NOT NULL,
   cidade VARCHAR(50) NOT NULL,
   estado VARCHAR(50) NOT NULL,
   cep VARCHAR(20) NOT NULL,
-  pais VARCHAR(100) NOT NULL,
-  CONSTRAINT fk_endereco_cliente FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente)
+  pais VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+  id_users SERIAL PRIMARY KEY,
+  nome_completo VARCHAR(100) NOT NULL,
+  login VARCHAR(100) NOT NULL,
+  email VARCHAR(30) NOT NULL,
+  cpf VARCHAR(14) NOT NULL,
+  dt_nasc DATE,
+  endereco_id INT,  -- Adicionando a coluna para referenciar o ID do endereço
+  cargo TEXT NOT NULL,
+  senha VARCHAR(100),
+  CONSTRAINT fk_users_endereco FOREIGN KEY (endereco_id) REFERENCES endereco (id_endereco)
 );
 
 
-CREATE TABLE IF NOT EXISTS produto (
-    id_produto SERIAL PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    descricao TEXT,
-    preco NUMERIC(10, 2) NOT NULL,
-    estoque INT NOT NULL,
-    categoria VARCHAR(50) NOT NULL
+
+CREATE TABLE IF NOT EXISTS carrinho(
+    id_carrinho SERIAL PRIMARY KEY,
+    id_users INT NOT NULL,
+    id_produto INT NOT NULL,
+    quantidade INT NOT NULL,
+    CONSTRAINT fk_carrinho_users FOREIGN KEY (id_users) REFERENCES users (id_users),
+    CONSTRAINT fk_carrinho_produto FOREIGN KEY (id_produto) REFERENCES produto (id_produto)
 );
 
 
-CREATE TABLE IF NOT EXISTS entrega (
-  id_entrega SERIAL PRIMARY KEY,
-  id_cliente INT NOT NULL,
-  id_produto INT NOT NULL,
-  id_endereco INT NOT NULL,
-  tipo_entrega VARCHAR(100) NOT NULL,
-  taxa DECIMAL(6,2) NOT NULL,
-  CONSTRAINT fk_entrega_cliente FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente),
-  CONSTRAINT fk_entrega_produto FOREIGN KEY (id_produto) REFERENCES produto (id_produto),
-  CONSTRAINT fk_entrega_endereco FOREIGN KEY (id_endereco) REFERENCES endereco (id_endereco)
-);
-
-CREATE TABLE IF NOT EXISTS carrinho (
-  id_carrinho SERIAL PRIMARY KEY,
-  id_cliente INT NOT NULL,
-  id_produto INT NOT NULL,
-  quantidade INT NOT NULL,
-  CONSTRAINT fk_carrinho_cliente FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente),
-  CONSTRAINT fk_carrinho_produto FOREIGN KEY (id_produto) REFERENCES produto (id_produto)
-);
-
-CREATE TABLE IF NOT EXISTS categoria (
-  id_categoria SERIAL PRIMARY KEY,
-  nome VARCHAR(50) NOT NULL,
-  descricao TEXT NOT NULL
-);
+-- Alter column "cargo" in table "users" to smallint
+ALTER TABLE users
+ALTER COLUMN cargo TYPE smallint USING CASE WHEN cargo = 'admin' THEN 1 ELSE 0 END;
 
